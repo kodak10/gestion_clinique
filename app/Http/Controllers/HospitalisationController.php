777b374483a5e9470aa28ecd86a066459942e562
Kodak10 <atchinaymard10@gmail.com>
2025-08-18 23:11:41 +0000

    fix mise a our de la facture et le montant dans reglement

--- app/Http/Controllers/HospitalisationController.php
+++ app/Http/Controllers/HospitalisationController.php
@@ -589,7 +589,9 @@ class HospitalisationController extends Controller
             'total' => $validatedData['total'],
             'ticket_moderateur' => $validatedData['ticket_moderateur'],
             'montant_a_paye' => $validatedData['montant_a_paye'],
-            'reste_a_payer' => $validatedData['montant_a_paye'] - ($validatedData['caution'] ?? 0),
+            // 'reste_a_payer' => $validatedData['montant_a_paye'] - ($validatedData['caution'] ?? 0),
+
+            'reste_a_payer' => $validatedData['montant_a_paye'] ?? 0,
             'reduction' => $validatedData['reduction'] ?? 0,
             'reduction_par' => $validatedData['reduction_par'] ?? null,
             'user_id' => auth()->id(),
